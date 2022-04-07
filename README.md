d2de2ef3f358190e01918c3b359ca1078a9fced0
Piyush Raj <piyush.raj@walmart.com>
2022-04-07 12:17:13 +0530

    Updated readme and added verbosity

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# log-parser
+<p align="center"><img src="https://raw.githubusercontent.com/devops-pr/walmart_hackathon/develop/media/walmart_logo.png" width="328px"><p>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+![Python](https://img.shields.io/badge/python-v3.6-blue.svg)
+![Dependencies](https://img.shields.io/badge/dependencies-up%20to%20date-brightgreen.svg)
+<br><br>
+
+<p align="center"><img src="https://raw.githubusercontent.com/devops-pr/walmart_hackathon/develop/media/walmart-screenshot.png" width="100%"><p>
+
+#LOG PARSER
+## Features
+- Parse a log file and generate detailed report as below:
+
+    - What are the number of requests served by day?
+    - What are the 3 most frequent User Agents by day?
+    - What is the ratio of GET's to POST's by OS by day?
+   
+- If executed in verbose mode with -v, it provides additional detailed report about the traffic pattern.
+
+## Setup
+``` 
+git clone https://github.com/devops-pr/log-parser.git
+cd log-parser
+python3 -f <log_file> [-v]
+```
--- log_parser.py
+++ log_parser.py
@@ -1,10 +1,26 @@
-from pprint import pprint
+#!/usr/bin/env python3
+""" Generate a detailed report from a log file.
+Usage:
+     python3 log_parser.py -f <logfile location>
+"""
 
-def request_summary():
+__author__ = "Piyush Raj"
+__version__ = "1.0.0"
+__email__ = "piyushraj@outlook.in"
+
+
+# ================================== Load required modules and methods =======================================#
+
+from pprint import pprint
+import argparse
+# =========================================== Function definition =========================================== #
+
+
+def request_summary(log_file):
     invalid_log_count = 0
     data = {}
     try:
-        with open("sample.log", "r") as f:
+        with open(log_file, "r") as f:
             for line in f:
                 try:
                     date = line.split("[")[1].split("]")[0].split(" ")[0].split(":")[0]
@@ -12,19 +28,23 @@ def request_summary():
                     user_agent = line.split('"')[5]
                     method = line.split('"')[1].split(" ")[0]
                     if date not in data:
-                        data[date] = {"request_summary": {"request_count": 1, status_code: 1, method: 1},"user_agent_summary": {user_agent: 1}}
+                        data[date] = {"request_summary": {"request_count": 1, status_code: 1, method: 1},
+                                      "user_agent_summary": {user_agent: 1}}
                     else:
-                        data[date]["request_summary"]["request_count"] = data[date]["request_summary"]["request_count"] + 1
+                        data[date]["request_summary"]["request_count"] = \
+                            data[date]["request_summary"]["request_count"] + 1
                         if method in data[date]["request_summary"]:
                             data[date]["request_summary"][method] = data[date]["request_summary"][method] + 1
                         else:
                             data[date]["request_summary"][method] = 1
                         if status_code in data[date]["request_summary"]:
-                            data[date]["request_summary"][status_code] = data[date]["request_summary"][status_code] + 1
+                            data[date]["request_summary"][status_code] = \
+                                data[date]["request_summary"][status_code] + 1
                         else:
                             data[date]["request_summary"][status_code] = 1
                         if user_agent in data[date]["user_agent_summary"]:
-                            data[date]["user_agent_summary"][user_agent] = data[date]["user_agent_summary"][user_agent] + 1
+                            data[date]["user_agent_summary"][user_agent] = \
+                                data[date]["user_agent_summary"][user_agent] + 1
                         else:
                             data[date]["user_agent_summary"][user_agent] = 1
                 except IndexError:
@@ -36,9 +56,10 @@ def request_summary():
 
 def req_per_day(data):
     required_data = {}
-    for k,v in data.items():
+    for k, v in data.items():
         required_data[k] = v["request_summary"]["request_count"]
     return required_data
+
 
 def user_agent_report(data):
     required_data = {}
@@ -48,6 +69,7 @@ def user_agent_report(data):
         ordered_dict = {k: v for v, k in sorted_value_key_pairs[0:3]}
         required_data[date] = ordered_dict
     return required_data
+
 
 def get_to_post_ratio(data):
     required_data = {}
@@ -59,12 +81,20 @@ def get_to_post_ratio(data):
     return required_data
 
 
+# =========================================== Begining of Execution =========================================== #
 
 if __name__ == '__main__':
-    invalid_logs, traffic_summary = request_summary()
-    # print(traffic_summary)
-    if invalid_logs >0 :
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--log_file', default="sample.log")
+    parser.add_argument("-v", "--verbose", help="modify output verbosity", action="store_true")
+    args = parser.parse_args()
+
+    invalid_logs, traffic_summary = request_summary(args.log_file)
+    if invalid_logs > 0:
         print("Invalid log entries count: " + str(invalid_logs))
+    if args.verbose == True:
+        print("########## Printing detailed traffic summary:")
+        pprint(traffic_summary)
     print("########## Number of requests served by day:")
     pprint(req_per_day(traffic_summary))
     print("\n########## 3 most frequent User Agents by day:")
